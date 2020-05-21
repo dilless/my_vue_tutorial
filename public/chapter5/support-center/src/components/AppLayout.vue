e7cee3465d0fd049614a2dfb7408d17368c34385
dilless <bohuang1994@gmail.com>
2020-05-21 17:27:14 +0800

    20200521xiaban

---
+++ public/chapter5/support-center/src/components/AppLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="app-layout">
+      <header class="header">
+        <div>
+          <img src="../assets/logo.svg" alt="" class="img">
+        </div>
+        <div>My shirt shop</div>
+      </header>
+    </div>
+
+  <router-view/>
+</template>
+
+<script>
+    export default {
+        name: "AppLayout"
+    }
+</script>
+
+<style lang="stylus" scoped>
+@import "../style/main.styl"
+</style>
+
+<style lang="stylus" scoped>
+  .header
+    .img
+      width: 64px
+      height: 64px
+</style>
